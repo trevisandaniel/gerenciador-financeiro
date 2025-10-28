b6f9e6ff7779a97fccbba46196c5155aed40f818
Daniel Trevisan <daniel-2193@hotmail.com>
2025-10-27 21:23:53 -0300

    Computed: lógica no scss

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -12,4 +12,16 @@ export class BalanceCard {
   type = input.required<'income' | 'outcome' | 'balance'>();
   label = input.required<string>();
   value = input.required<number>();
+
+  cssClass = computed(() => {
+    if(this.type() === 'income') {
+      return 'income';    
+    }
+
+    if(this.type() === 'outcome') {
+      return 'outcome';    
+    }
+
+    return this.value() > 0 ? 'income' : 'outcome';
+  })
 }
