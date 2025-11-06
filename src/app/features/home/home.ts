770d8a2941757f8e29f9f2ca568e207420edd6a0
Daniel Trevisan <daniel-2193@hotmail.com>
2025-11-05 21:23:27 -0300

    Mostrar mensagem quando não há transações

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,10 +3,11 @@ import { Balance } from "./components/balance/balance";
 import { TransactionItem } from "./components/transaction-item/transaction-item";
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
 import { TransactionType } from '../../shared/transaction/enums/transaction-type';
+import { NoTransactions } from "./components/no-transactions/no-transactions";
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
