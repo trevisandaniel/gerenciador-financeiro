56b98051f5614b3f95ccb912780f821434d5c1d1
Daniel Trevisan <daniel-2193@hotmail.com>
2025-11-16 09:57:51 -0300

    Melhorando formulário e adicionando validações

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,11 +1,14 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatAnchor, MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-create',
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatInputModule,
+    ReactiveFormsModule, JsonPipe, MatAnchor, MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
@@ -13,6 +16,11 @@ export class CreateComponent {
 
   form = new FormGroup({
     title: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    value: new FormControl('', {
       validators: [
         Validators.required
       ]
