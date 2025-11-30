579d7835365656e50d0033d964fba2afd89c0afb
Daniel Trevisan <daniel-2193@hotmail.com>
2025-11-30 09:48:44 -0300

    Adicionando button-toggle ao formulário de transação

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,20 +1,32 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatAnchor, MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleGroup, MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatAnchor, MatButtonModule } from "@angular/material/button";
+import { TransactionType } from '../../../../shared/transaction/enums/transaction-type';
 
 @Component({
   selector: 'app-create',
-  imports: [MatFormFieldModule, MatInputModule,
-    ReactiveFormsModule, JsonPipe, MatAnchor, MatButtonModule],
+  imports: [
+    MatFormFieldModule, MatInputModule,
+    ReactiveFormsModule, JsonPipe,
+    MatAnchor, MatButtonModule, 
+    MatButtonToggleGroup, MatButtonToggleModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
 export class CreateComponent {
+  readonly transactionTypes = TransactionType;
 
   form = new FormGroup({
+    type: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
     title: new FormControl('', {
       validators: [
         Validators.required
